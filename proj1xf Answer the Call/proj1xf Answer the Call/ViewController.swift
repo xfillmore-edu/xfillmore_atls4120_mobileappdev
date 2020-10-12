d3338b457372f3e81654d1d700341cc02b048acc
xfillmore <47928405+xfillmore@users.noreply.github.com>
2020-10-12 17:04:53 -0600

    init proj 1 w appicons and launchscreen

---
+++ proj1xf Answer the Call/proj1xf Answer the Call/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proj1xf Answer the Call
+//
+//  Created by Xuedan Fillmore on 10/12/20.
+//  Copyright © 2020 Xuedan Fillmore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
